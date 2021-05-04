b254ae61c975c98b59a8033a062722683db96993
xtony77 <xtony77@gmail.com>
2021-05-04 14:57:47 +0800

    feat: add frontend script echo message

--- frontend/scripts/docker_entrypoint.sh
+++ frontend/scripts/docker_entrypoint.sh
@@ -8,5 +8,8 @@ cp src/config/config.prod.json src/config/config.json
 sed -i 's@NodeRPC@'${NodeRPC}'@' src/config/config.json
 
 if [ ! -f ./build/index.html ]; then
+    echo "frontend building ..."
     npm run build
+    echo "frontend build finish."
 fi
+echo -e "\nAccess the UI via http://localhost\n"
